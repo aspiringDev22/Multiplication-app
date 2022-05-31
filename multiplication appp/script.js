8ae555be2f5f23ffa6e31bf1b029ebcefa882d53
Anirudh <102802211+aspiringDev22@users.noreply.github.com>
2022-05-31 09:03:22 -0700

    Add files via upload

---
+++ multiplication appp/script.js
@@ -0,0 +1,43 @@
+const num1 = Math.floor(Math.random() * 10);
+// console.log(num1);
+
+const num2 = Math.floor(Math.random() * 10);
+// console.log(num2);
+
+const questionEl = document.getElementById("question");
+
+const inputEl = document.getElementById("input");
+
+const formEl = document.getElementById("form");
+
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  //   console.log(userAns, typeof userAns);
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+    // console.log(score);
+  } else {
+    score--;
+    // console.log(score);
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
